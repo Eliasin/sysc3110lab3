a58f272c4230b5ef2e9d30742d61b8113d5d42d4
Steven <steven.pham.8.2000@gmail.com>
2021-09-17 11:21:56 -0400

    Change AddressBook main to test case

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -17,6 +17,10 @@ public class AddressBook {
     }
 
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        BuddyInfo buddyInfo = new BuddyInfo("Jim", "Mimsy", "6138888888");
+
+        AddressBook myAddressBook = new AddressBook();
+        myAddressBook.addBuddy(buddyInfo);
+        myAddressBook.removeBuddy(buddyInfo);
     }
 }
